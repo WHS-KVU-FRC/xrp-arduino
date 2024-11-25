6764123b2d32085e4e822f4e6ba80fbdd4019829
ktvuwhs <ktvu@hbuhsd.edu>
2024-11-25 15:24:00 -0800

    message 1 for teaching

--- src/XRPEncoder.cpp
+++ src/XRPEncoder.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
 #include "XRPEncoder.h"
 
+// Changed code for teaching purposes
+
 xrp::XRPEncoder::XRPEncoder(int channelA, int channelB) {
   this->channelA = channelA;
   this->channelB = channelB;
