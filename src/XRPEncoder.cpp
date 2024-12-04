6e5e69cecdae5abab7f4f014eae827dc4d724043
ktvuwhs <ktvu@hbuhsd.edu>
2024-12-04 12:08:39 -0800

    using external pico encoder library

--- src/XRPEncoder.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arduino.h"
-#include "XRPEncoder.h"
-
-// Changed code for teaching purposes
-
-xrp::XRPEncoder::XRPEncoder(int channelA, int channelB) {
-  this->channelA = channelA;
-  this->channelB = channelB;
-}
